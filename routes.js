c134070f6a186dec56c862e4e51099219bac2228
Krisna173 <krisnagunayanuar.met@gmail.com>
2025-02-28 10:16:42 +0700

    memperbaiki server js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = function(app){
+export default function(app) {
     var jsonku = require('./controller');
     
     app.route('/')
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express');  // Tambahkan ini!
-const bodyParser = require('body-parser'); // Tambahkan ini!
+const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
 const app = express();
 
-// Parses application/json
+// Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 // Panggil routes
-const routes = require('./routes');
-routes(app);
+const routes = require('./routes'); // Import dengan benar
+routes(app); // Pastikan ini dipanggil sebagai fungsi
 
-// Daftarkan menu routes dari index
-app.use('/auth', require('./middleware')); // Pastikan 'middleware.js' ada
+// Middleware tambahan
+app.use('/auth', require('./middleware'));
 
 app.listen(8080, () => {
     console.log('Server started on port 8080');
